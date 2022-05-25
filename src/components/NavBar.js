2cc61f3d2fc04dc23b9dc293260092856042d130
Markus D <markusdrx@gmail.com>
2022-05-25 21:10:43 +0200

    started NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,21 @@
 import React from "react";
 
 const NavBar = () => {
+  const user = { name: "Markus" };
+
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <nav className="navbar navbar-dark bg-dark mb-4">
+      <div className="container">
+        <span className="navbar-brand">
+          <h2>{user ? `Hi, ${user.name}!` : `Welcome!`} </h2>
+        </span>
+        {user ? (
+          <button className="btn btn-primary">Logoff</button>
+        ) : (
+          <button className="btn btn-primary">Login</button>
+        )}
+      </div>
+    </nav>
   );
 };
 
